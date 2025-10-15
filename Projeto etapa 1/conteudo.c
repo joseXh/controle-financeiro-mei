c21138442bb8699397edb508943667e80b9b16ae
joseXh <josexhozk@gmail.com>
2025-10-14 21:29:52 -0300

    Add files via upload

---
+++ Projeto etapa 1/conteudo.c
@@ -0,0 +1,13 @@
+/* 
+ Projeto: controle_financeiro_mei
+ Autores: João Victor Cambraia Nogueira e José Emanuel
+Data de criação: [14/10/2025]
+ Descrição: Estrutura inicial do projeto integrador.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Iniciando o Projeto Integrador...\n");
+    return 0;
+}
